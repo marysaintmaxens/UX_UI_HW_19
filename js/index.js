139571107edc548688c37d84bcf7442b70e17a42
marysaintmaxens <mary.saint.maxens@gmail.com>
2024-02-06 12:29:25 -0500

    updates
    adding images and hovering effects

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function(){
+    $("#resume").click(function(){
+        $(this).css("background-color", "white");
+    });
+});
